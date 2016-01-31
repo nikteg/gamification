639312c5dd50c291cd3125da3ec493fdabf4add7
Niklas Tegnander <bipshark@gmail.com>
2016-01-31 14:53:22 +0100

    Add /auth/login and /auth/create routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,18 +5,52 @@ import convert from 'koa-convert'
 import required from '../util/required'
 import config from '../../config'
 import conn from '../conn'
+import { hash } from 'crypto-promise'
 
 const auth = new Router()
 
 auth.use(convert(koabody()))
 
-auth.post('/', async (ctx, next) => {
+auth.post('/login', async (ctx, next) => {
   let params = required('username', 'password')(ctx.request.body)
 
-  let result = await conn.one('SELECT username FROM "user" WHERE username = $1', params.username)
-  let token = koajwt.sign({ username: result.username }, config.secret)
+  const h = await hash('md5')(params.password)
 
-  ctx.body = { token: token }
+  try {
+    let result = await conn.one('SELECT id, username FROM "user" WHERE username = ${username} AND password = ${hash}', {
+      username: params.username,
+      hash: h.toString('hex')
+    })
+
+    let token = koajwt.sign({ id: result.id, username: result.username }, config.secret)
+
+    ctx.body = { token: token }
+  } catch (err) {
+    if (err.message.includes('No data')) {
+      throw new Error('Invalid username and/or password')
+    }
+  }
+})
+
+auth.post('/create', async (ctx, next) => {
+  let params = required('username', 'password')(ctx.request.body)
+
+  const h = await hash('md5')(params.password)
+
+  try {
+    await conn.none('INSERT INTO "user" (username, password) VALUES (${username}, ${hash})', {
+      username: params.username,
+      hash: h.toString('hex')
+    })
+
+    ctx.body = 'OK'
+  } catch (err) {
+    if (err.message.includes('duplicate key')) {
+      if (err.message.includes('username')) {
+        throw new Error('Username already taken')
+      }
+    }
+  }
 })
 
 export default auth
