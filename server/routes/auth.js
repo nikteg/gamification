d5d33b4a9d3d390900ea555ffc066dbac5d09975
Niklas Tegnander <bipshark@gmail.com>
2016-01-31 22:44:26 +0100

    async await -> promises

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,50 +4,55 @@ import required from '../util/required'
 import config from '../../config'
 import conn from '../conn'
 import { hash } from 'crypto-promise'
+import throwDebug from '../util/throwDebug'
+
+const hashPassword = (password) => hash('md5')(password).toString('hex')
+
+const getUser = async (username, password) => {
+  return conn.one('SELECT id, username FROM "user" WHERE username = ${username} AND password = ${hash}', {
+    username: username,
+    hash: await hashPassword(password)
+  })
+}
+
+const createUser = async (username, password, email) => {
+  return conn.one('INSERT INTO "user" (username, password) VALUES (${username}, ${hash}) RETURNING id, username', {
+    username: username,
+    hash: await hashPassword(password)
+  })
+}
+
+const tokenize = (id, username) => koajwt.sign({ id: id, username: username }, config.secret)
 
 const auth = new Router()
 
-
 auth.post('/login', async (ctx, next) => {
-  let params = required('username', 'password')(ctx.request.body)
+  const params = required('username', 'password')(ctx.request.body)
 
-  const h = await hash('md5')(params.password)
+  await getUser(params.username, params.password)
+    .then(res => ctx.body = { token: tokenize(res.id, res.username) }, err => {
+      if (err.message.includes('No data')) {
+        throw new Error('Invalid username and/or password')
+      }
 
-  try {
-    let result = await conn.one('SELECT id, username FROM "user" WHERE username = ${username} AND password = ${hash}', {
-      username: params.username,
-      hash: h.toString('hex')
+      throwDebug(err)
     })
-
-    let token = koajwt.sign({ id: result.id, username: result.username }, config.secret)
-
-    ctx.body = { token: token }
-  } catch (err) {
-    if (err.message.includes('No data')) {
-      throw new Error('Invalid username and/or password')
-    }
-  }
 })
 
 auth.post('/create', async (ctx, next) => {
-  let params = required('username', 'password')(ctx.request.body)
+  console.log(ctx.request.body)
+  const params = required('username', 'password')(ctx.request.body)
 
-  const h = await hash('md5')(params.password)
-
-  try {
-    await conn.none('INSERT INTO "user" (username, password) VALUES (${username}, ${hash})', {
-      username: params.username,
-      hash: h.toString('hex')
-    })
-
-    ctx.body = 'OK'
-  } catch (err) {
-    if (err.message.includes('duplicate key')) {
-      if (err.message.includes('username')) {
-        throw new Error('Username already taken')
+  await createUser(params.username, params.password)
+    .then(res => ctx.body = { token: tokenize(res.id, res.username) }, err => {
+      if (err.message.includes('duplicate key')) {
+        if (err.message.includes('username')) {
+          throw new Error('Username already taken')
+        }
       }
-    }
-  }
+
+      throwDebug(err)
+    })
 })
 
 export default auth
