fbba29cd363b0c21db721494a0c12960e394de4f
tobiasandersen <j.tobiasandersen@gmail.com>
2016-02-07 22:05:30 +0100

    Update index and main.js according to new structure

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { useRouterHistory } from 'react-router'
 import { createHistory } from 'history'
 import routes from './routes'
 import Root from './containers/Root'
-import configureStore from './redux/configureStore'
+import configureStore from './store/configureStore'
 
 const historyConfig = { basename: __BASENAME__ }
 const history = useRouterHistory(createHistory)(historyConfig)
@@ -12,7 +12,6 @@ const history = useRouterHistory(createHistory)(historyConfig)
 const initialState = window.__INITIAL_STATE__
 const store = configureStore({ initialState, history })
 
-// Render the React application to the DOM
 ReactDOM.render(
   <Root history={history} routes={routes} store={store} />,
   document.getElementById('root')
