83f0345d8aa3e911f5f37d2c24f9c9a37d799a8a
Alex Tao <talex@student.chalmers.se>
2016-05-02 21:42:38 +0200

    Updated texts for experiment 1 and 2

--- src/courses/1/chapters/1/exercise2.js
+++ src/courses/1/chapters/1/exercise2.js
@@ -6,9 +6,8 @@ export default () => (
       <h3 className="Experiment-subTitle">General information</h3>
 
       <p>
-        The first step of a statistical experiment is deciding on what
-        characteristics of a subject one wants to observe and analyze.
-        The the subject must fulfill these points in order to conduct such an experiment:
+       The first step of a statistical experiment is deciding on the subject to experiment on. 
+	   The subject must fulfill these points in order for such an experiment to be successful:
       </p>
 
       <ol className="Experiment-listMargin">
@@ -22,9 +21,12 @@ export default () => (
     <div className="Experiment-part">
       <h3 className="Experiment-subTitle">This experiment</h3>
 
-      <p>In this specific experiment, the subject we choose to observe is the height of different human individuals. It can be narrowed down even further by defining a gender or nationality. However we will not do that in this experiment, instead we compare the heights of human beings despite of their background or gender.</p>
+      <p>In this specific experiment, the subject we choose to observe is the height of different human individuals. 
+	  It can be narrowed down even further by defining a gender, nationality or age. However we will not do that in this experiment, 
+	  instead we will compare the heights of people despite of their background or gender.</p> 
 
-      <p>To be sure that we can actually use the human height as an experiment, we need to check whether the experiment fulfills the four points above. Does it?</p>
+      <p>To be sure that we can actually use the human height as an experiment, 
+	  we need to check whether the experiment fulfills the four points above. Does it?</p>
     </div>
 
     <div className="Experiment-answer">
@@ -39,13 +41,15 @@ export default () => (
           Each outcome can be determined, we know that a person’s height should probably be between 0 and 250 cm. Let’s set the maximum to 300 cm.
         </li>
         <li>
-          The outcome of the subject is actually dependent many different factors such as genetics, environment, etc. However, all factors combined are so complex that it may be considered to be dependent on probability.
+          The outcome of the subject is actually dependent many different factors such as genetics, environment, etc. 
+		  However, all factors combined are so complex that it may be considered to be dependent on probability.
         </li>
       </ol>
     </div>
 
     <div className="Experiment-part">
-      <p className="Experiment-question">Once we have concluded that the subject is fit for conducting a statistical experiment, we can start finding out what questions we want to answer. In this case we may be curious about:
+      <p className="Experiment-question">Once we have concluded that the subject is fit for conducting a statistical experiment.
+	  Now there are a few questions we may want to find the answers to. Here are a few examples:
       </p>
       <ul className="Experiment-listMargin">
         <li className="Experiment-question">What is the most common height?</li>
--- src/courses/1/chapters/1/exercise3.js
+++ src/courses/1/chapters/1/exercise3.js
@@ -4,34 +4,49 @@ export default () => (
   <div>
     <div className="Experiment-part">
       <h3 className="Experiment-subTitle">General information</h3>
-      <p>Now it is time to perform the statistical experiment from the previous step iteratively until enough data has been gathered. The more samples gathered, the more accurate the information will be. When measuring attributes of the subject of the experiment one should be mindful of the fact that certain interval of errors of measurement will occur.</p>
-      <p>Graphing can be done once enough data has been gathered. Let X be a random variable containing the distribution of the subject and x be one of the values of X. The sample distribution can then be graphed by:</p>
+      <p>Now it is time to perform the statistical experiment from the previous step iteratively until enough data has been gathered. 
+	  The larger the sample size, the more accurate conclusions we can draw from the data. When measuring attributes of the subject of the experiment one 
+	  should be mindful of the fact that certain interval of errors in measurement will occur.</p>
+	  <p>Graphing can be done once we have gathered our random sample. Let X be a random variable containing the
+	  distribution of the subject and x be one of the values of X. The sample distribution can then be graphed by: </p>
       <p>Y axis: frequency of X = x</p>
       <p>X axis: x</p>
-      <p>Whether X is discrete or continuous, it is often necessary to group the data in intervals to make sure the data isn’t too spread out, especially if the sample size is small in comparison to its variance. The most common groups are 10 integer intervals (e.g the group 1 contains all values from -5 to 5) and 1 integer intervals (e.g the group 1 contains -0.5 to 0.5).</p>
-    </div>
+      <p>Whether X is discrete or continuous, it is often necessary to group the sample units in 
+	  intervals to make sure the data on the graph isn’t too spread out, especially if the sample size is small in comparison 
+	  to its variance. The most common groups are 10 integer intervals (e.g the interval located at x = 0 contains all values of x from -5 to 
+	  5) and 1 integer intervals (e.g the interval located at x = 0 contains all values of x from -0.5 to 0.5). </p>
+	  </div>
 
     <div className="Experiment-part">
       <h3 className="Experiment-subTitle">This experiment</h3>
-      In this experiment, we would begin with measuring the height of different people. Luckily, we already have a dataset of measured heights available to us.
+      <p>In this experiment, we would begin with measuring the height of different people. Luckily, we already have a dataset of measured heights available to us.</p>
       <a href="http://socr.ucla.edu/docs/resources/SOCR_Data/SOCR_Data_Dinov_020108_HeightsWeights.html">Link to dataset</a>
-      If you take a look at the data-set, you can see that the dataset has a size of about 20 000 measures. With some educated guesses, we conclude that the height would probably range from about 140 to 240 cm. This means the variance is rather small in contrast to the sample size. Thus, we decide to group the values to 1 integer.
-      Let us graph it by frequency on the y axis and by height on the x axis:
-    </div>
+      <p>If you take a look at the data-set, you can see that the dataset has a size of about 20 000 measures. 
+	  With some educated guesses, we conclude that the heights would probably range from about 140 to 240 cm. 
+	  his means the variance is rather small in contrast to the sample size. Thus, we decide to group the values to 1 integer. </p>	
+	  <p>To the right we have an interactive bar chart. Use it to explore the distribution of the data with different sample sizes.</p>
+	  <p>Instructions:</p>
+	  <ul>
+		<li>To add more sample units to the graph, press “Add sample units”.
+		<li>In order to change the amount of new sample units added, use the slider to the left of the button. 
+		<li>By hovering the mouse over a bar it shows the x and y value of the bar on top of the bar.
+		<li>By pressing a hovered bar, it shows the cumulative probability distribution P[X < x], where x is the x value of the bar clicked on.
+	  </ul>
+  </div>
 
     <div className="Experiment-answer">
-      Experiments and observations
+      <p>Experiments and observations</p>
       <ol className="Experiment-listMargin">
-        <li>How does the distribution look when we use a smaller sample size?</li>
-        <li>What would happen if we had an even bigger sample size?</li>
-        <li>What happens if we do not round the data(Imagine we have infinite decimals)? Why does this happen?</li>
-        <li>What happens if we round the data too much?</li>
+        <li>How does the distribution look when we use a small sample size? Say, 25.</li>
+        <li>What happens when we use a  bigger sample size? Say, 5000.</li>
+        <li>What would happen if we don’t group the data(Imagine each sample unit has infinite decimals)? Why does this happen?</li>
+        <li>What happens if we group the data too much?</li>
         <li>How is the data distributed? What kind of distribution does it resemble?</li>
         <li>Is the distribution continuous or discrete? How do we know?</li>
-        <li>What is the probability that a person is less than 180 cm tall (according to our samples)?</li>
-        <li>What is the probability that a person is more than 200 cm tall (according to our samples)?</li>
-        <li>What is the probability that a person is between 140-160 cm tall (according to our samples)? </li>
-        <li>Is it possible to find out the probability to be less than 180.001 cm tall? </li>
+        <li>What is the probability that a person is less than 180 cm tall (according to our random sample)?</li>
+        <li>What is the probability that a person is more than 200 cm tall (according to our random sample)?</li>
+        <li>What is the probability that a person is between 140-160 cm tall (according to our random sample)? </li>
+        <li>Is it possible to find out the probability for a person to be exactly 150 cm tall with this graph? Why or why not? </li>
       </ol>
     </div>
 
